188050b5beb87f52fc4fa7f792afe0f9fc025798
RyuTKC <ddukes.tk@gmail.com>
2022-10-25 00:09:44 +0900

    first commit

---
+++ react-login-test/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Home from './componets/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Frebaseでログイン機能を実装しよう</h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
